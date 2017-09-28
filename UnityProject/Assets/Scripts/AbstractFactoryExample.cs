d0b439aa0bcc5a3d166ee7c24902fd51c5608239
aaronvark <aoostdijk@gmail.com>
2017-09-28 11:29:16 +0200

    Added theory 4 lesson code and additional examples.

---
+++ UnityProject/Assets/Scripts/AbstractFactoryExample.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractFactoryExample : MonoBehaviour {
+	void Start() {
+		AbstractFactory instance = AbstractFactory.GetInstance();
+		instance.DoSomething();
+	}
+}
